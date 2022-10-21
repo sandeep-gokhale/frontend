2f2e35f1769557cddd4ece609683e6a0ed7a984b
sandeep-gokhale <sandeep.gokhale@techvito.in>
2022-10-21 14:05:12 +0530

    Commit frontend code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets Learn CI/CD via Node and React Application hosted on AWS via github actions.
         </a>
       </header>
     </div>
