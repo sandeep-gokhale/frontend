c0d67630d8a702a1a27bb5250df576673ec9dd40
sandeep-gokhale <sandeep.gokhale@techvito.in>
2022-11-11 09:32:00 +0530

    Initial Change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
 function App() {
+  // React.useEffect(() => {
+  //   fetch("http://127.0.0.1:80/")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +22,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lets Learn CI/CD via Node and React Application hosted on AWS via github actions.
+          Lets Learn CI/CD via Node and React Application hosted on AWS via
+          github actions.
         </a>
       </header>
     </div>
