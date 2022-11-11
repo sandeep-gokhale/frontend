217d6d6fe0ff4e05b03951f44cdf31763bd7d96b
sandeep-gokhale <sandeep.gokhale@techvito.in>
2022-11-11 10:34:09 +0530

    Updates Package.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-// module.exports = function (app) {
-//   console.log("inside...");
-//   app.use(
-//     "/api",
-//     createProxyMiddleware({
-//       target: "http://127.0.0.1:80",
-//       changeOrigin: true,
-//     })
-//   );
-// };
+module.exports = function (app) {
+  console.log("inside...");
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://127.0.0.1:80",
+      changeOrigin: true,
+    })
+  );
+};
